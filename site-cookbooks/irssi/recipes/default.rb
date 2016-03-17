f1a96dc363d83deddad37fce80ded3f542999908
Arvid Warnecke <arvid.warnecke@otto.de>
2016-03-17 21:55:34 +0100

    [irssi] Move scripts and install them

--- site-cookbooks/irssi/recipes/default.rb
+++ site-cookbooks/irssi/recipes/default.rb
@@ -10,4 +10,15 @@ cookbook_file "/home/#{node[:irssi][:user]}/.irssi/config" do
   action :create
 end
 
-
+%w{ adv_windowlist.pl chanshare.pl dccstat.pl hilightwin.pl keepnick.pl
+    notonline.pl autorealname.pl country.pl friends.pl irssinotifier.pl
+    nickcolor.pl title.pl
+  }.each do |file|
+  cookbook_file "/home/#{node[:irssi][:user]}/.irssi/#{file}" do
+    source "plugins/#{file}"
+    owner "#{node[:irssi][:user]}"
+    group "#{node[:irssi][:user]}"
+    mode '0644'
+    action :create
+  end
+end
