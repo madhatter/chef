2a0a4e306e49b8ade3a5423ee7225828acd34fc4
Arvid Warnecke <madhatter@nostalgix.org>
2016-03-25 19:56:19 +0100

    [docker] Updated mount for registry

--- site-cookbooks/docker/recipes/registry.rb
+++ site-cookbooks/docker/recipes/registry.rb
@@ -5,6 +5,13 @@
 # Installs base docker setup
 #
 
+directory '/docker_repo' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
 template '/etc/systemd/system/docker-registry.service' do
   source 'docker_registry_unit.erb'
   owner 'root'
