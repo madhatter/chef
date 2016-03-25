59e24b1a2cb438ceffa81f81f3db3eced54c3df2
Arvid Warnecke <madhatter@nostalgix.org>
2016-03-25 21:00:59 +0100

    [base] Renamed from domestic

--- site-cookbooks/base/recipes/default.rb
+++ site-cookbooks/base/recipes/default.rb
@@ -1,9 +1,9 @@
 #
-# Cookbook Name: domestic
+# Cookbook Name: base
 # Recipe: default
 #
 # Creates a user with home and installs all the
 # necessary things to feel like home.
 # 
 
-include_recipe 'domestic::git'
+include_recipe 'base::git'
