026dfa7dd9b20ef700f441eeb7c5fe46b2a97d13
Arvid Warnecke <madhatter@nostalgix.org>
2016-03-26 12:51:14 +0100

    [docker] Added logfiles for cronjobs

--- site-cookbooks/docker/recipes/default.rb
+++ site-cookbooks/docker/recipes/default.rb
@@ -26,7 +26,7 @@ end
 
 # Remove all stopped containers once in a while
 cron 'Remove old docker containers' do
-  command 'docker ps -q | xargs docker stop || docker ps -a -q | xargs docker rm'
+  command 'docker ps -q | xargs docker stop || docker ps -a -q | xargs docker rm >/var/log/docker_cleanup_old_containers.log 2>&1'
   minute '0'
   hour '10,22'
   weekday '1'
@@ -34,8 +34,16 @@ cron 'Remove old docker containers' do
 end
 
 cron 'Remove dangling docker images' do
-  command  'docker images -q -f dangling=true | xargs docker rmi'
+  command  'docker images -q -f dangling=true | xargs docker rmi >/var/log/docker_cleanup_dangling_images.log 2>&1'
   minute '0'
+  hour '9,21'
+  weekday '*'
+  user 'root'
+end
+
+cron 'Remove untagged docker images' do
+  command  'docker images | grep "^<none>" | awk \'{print $3}\' | xargs docker rmi >/var/log/docker_cleanup_untagged_images.log 2>&1'
+  minute '30'
   hour '9,21'
   weekday '*'
   user 'root'
