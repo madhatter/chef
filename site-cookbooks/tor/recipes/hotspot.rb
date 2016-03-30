edd83c6a3ae5e241fad75f2705ec2d0a1fc52ea9
Arvid Warnecke <madhatter@nostalgix.org>
2016-03-30 22:18:38 +0200

    [tor] Added template for hostapd.conf and usage of databag

--- site-cookbooks/tor/recipes/hotspot.rb
+++ site-cookbooks/tor/recipes/hotspot.rb
@@ -3,3 +3,14 @@
     action :install
   end
 end
+
+wifi_secret = data_bag_item('production', 'passwords', 'tor')
+
+template '/etc/hostapd/hostapd.conf' do
+  source 'hostapd.conf.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  variables(:wifi_secret => wifi_secret)
+  action :create
+end 
