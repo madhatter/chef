eef10478eef8bc57e78cb36b3377cb1700c37ddb
madhatter <madhatter@nostalgix.org>
2016-03-31 17:12:13 +0200

    [tor] Fixed lookup of encrypted data bag item.

--- site-cookbooks/tor/recipes/hotspot.rb
+++ site-cookbooks/tor/recipes/hotspot.rb
@@ -4,13 +4,18 @@
   end
 end
 
-wifi_secret = data_bag_item('production', 'passwords', 'tor')
+wifi_secret = data_bag_item('production', 'passwords', IO.read('/etc/chef/encrypted_data_bag_secret'))
+
+log 'message' do
+  message "Secret: #{wifi_secret['tor']}"
+  level :info
+end
 
 template '/etc/hostapd/hostapd.conf' do
   source 'hostapd.conf.erb'
   owner 'root'
   group 'root'
   mode '0644'
-  variables(:wifi_secret => wifi_secret)
+  variables(:wifi_secret => wifi_secret['tor'])
   action :create
 end 
