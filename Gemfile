046b962388d9ff20e41991110c4b30d95a7ffbde
Arvid Warnecke <madhatter@nostalgix.org>
2016-04-13 22:52:57 +0200

    [base] Inital chefspec added.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,10 @@
 source 'https://rubygems.org'
 
-gem 'chef'
-gem 'knife-solo'
-gem 'librarian-chef'
-gem 'knife-solo_data_bag'
+gem 'chef', '~> 12.8.0'
+gem 'knife-solo', '~> 0.5.0'
+gem 'knife-solo_data_bag', '~> 1.1.0'
+gem 'librarian-chef', '~> 0.0.4'
+
+group :test do
+  gem 'chefspec', '~> 4.6.1'
+end
---
+++ site-cookbooks/base/spec/git_spec.rb
@@ -0,0 +1,14 @@
+require 'chefspec'
+
+describe 'base::git' do
+  let(:chef_run) do
+    ChefSpec::SoloRunner.new do |node|
+      node.set['user']['name'] = 'horst'
+    end.converge(described_recipe)
+  end
+
+  it 'creates git config files for a user' do
+    expect(chef_run).to create_template('/home/horst/.gitconfig')
+    expect(chef_run).to create_template('/home/horst/.gitignore')
+  end
+end
