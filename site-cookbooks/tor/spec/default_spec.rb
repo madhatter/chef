5023cdfd98df4d1c26a74b1989d6834b643434cc
Arvid Warnecke <madhatter@nostalgix.org>
2016-04-18 07:23:32 +0200

    [tor] Useless fiddlings to make specs work with librarian.

--- site-cookbooks/tor/spec/default_spec.rb
+++ site-cookbooks/tor/spec/default_spec.rb
@@ -4,6 +4,7 @@ describe 'tor::default' do
   let(:chef_run) do
     cookbook_paths = %W(#{File.expand_path('../../..', Dir.pwd)}/site-cookbooks #{File.expand_path('../../..', Dir.pwd)}/cookbooks)
     ChefSpec::SoloRunner.new({cookbook_path: cookbook_paths}) do |node|
+    #ChefSpec::SoloRunner.new() do |node|
       node.set['tor']['data_bag_dir'] = File.expand_path('./encrypted_data_bag_secret', File.dirname(__FILE__))
     end.converge(described_recipe)
   end
@@ -16,7 +17,7 @@ describe 'tor::default' do
     secret_file = File.expand_path('./encrypted_data_bag_secret', File.dirname(__FILE__))
     secret = Chef::EncryptedDataBagItem.load_secret(secret_file)
     allow(Chef::EncryptedDataBagItem).to receive(:load).with('production', 'passwords', secret).and_return(databag_stub)
-    allow(Kernel).to receive(:require).with('iptables-ng').and_return(true)
+    #allow(Kernel).to receive(:require).with('iptables-ng').and_return(true)
   end
 
   it 'installs dnsmasq' do
