cc338cdc85a3280883aa089cc140ff9cb301a930
Arvid Warnecke <madhatter@nostalgix.org>
2016-04-19 21:37:45 +0200

    [tor] Move specs for config files a bit around.

--- site-cookbooks/tor/spec/default_spec.rb
+++ site-cookbooks/tor/spec/default_spec.rb
@@ -32,7 +32,7 @@ describe 'tor::default' do
     end
   end
 
-  it 'creates hostapd config file with credentials' do
+  it 'creates config files with credentials' do
     expect(chef_run).to create_template('/etc/hostapd/hostapd.conf')
     expect(chef_run).to render_file('/etc/hostapd/hostapd.conf').with_content(/gammel/)
   end
@@ -41,20 +41,14 @@ describe 'tor::default' do
     expect(chef_run).to create_cookbook_file('/etc/systemd/system/openwifi.service')
   end
 
-  it 'creates tor config file' do
+  it 'creates more config files' do
     expect(chef_run).to create_cookbook_file('/etc/tor/torrc')
+    expect(chef_run).to create_cookbook_file('/etc/systemd/system/tor.service.d/start-as-root.conf')
+    expect(chef_run).to create_cookbook_file('/etc/sysctl.d/99-sysctl.conf')
   end
 
   it 'creates systemd service sub-directory' do
     expect(chef_run).to create_directory('/etc/systemd/system/tor.service.d')
-  end
-
-  it 'creates tor config file for systemd' do
-    expect(chef_run).to create_cookbook_file('/etc/systemd/system/tor.service.d/start-as-root.conf')
-  end
-
-  it 'creates sysctl config' do
-    expect(chef_run).to create_cookbook_file('/etc/sysctl.d/99-sysctl.conf')
   end
 
   it 'notifies sysctl' do
