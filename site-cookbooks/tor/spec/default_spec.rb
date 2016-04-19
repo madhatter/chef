1ba6ef8c87ac7d80b2fec5201f6077360fffce60
Arvid Warnecke <madhatter@nostalgix.org>
2016-04-19 07:14:32 +0200

    [tor] All not iptables related stuff spec'ed

--- site-cookbooks/tor/spec/default_spec.rb
+++ site-cookbooks/tor/spec/default_spec.rb
@@ -2,9 +2,18 @@ require_relative '../../spec_helper.rb'
 
 describe 'tor::default' do
   let(:chef_run) do
-    ChefSpec::SoloRunner.new do |node|
+    # Redirecting stdout to suppress iptables_ng_chain noise.
+    old_std_out = $stdout.clone
+    $stdout.reopen('tmp_std_out.txt', 'w')
+    result = ChefSpec::SoloRunner.new do |node|
       node.set['tor']['data_bag_dir'] = File.expand_path('./encrypted_data_bag_secret', File.dirname(__FILE__))
     end.converge(described_recipe)
+    $stdout.reopen(old_std_out)
+    File.read('tmp_std_out.txt').each_line do |lin|
+      puts lin unless lin =~ /iptables_ng_chain/
+    end
+    File.delete('tmp_std_out.txt')
+    result
   end
 
   databag_stub = {
@@ -21,5 +30,35 @@ describe 'tor::default' do
     it "installs #{package}" do
       expect(chef_run).to install_package(package)
     end
+  end
+
+  it 'creates hostapd config file with credentials' do
+    expect(chef_run).to create_template('/etc/hostapd/hostapd.conf')
+    expect(chef_run).to render_file('/etc/hostapd/hostapd.conf').with_content(/gammel/)
+  end
+
+  it 'creates openwifi unit file' do
+    expect(chef_run).to create_cookbook_file('/etc/systemd/system/openwifi.service')
+  end
+
+  it 'creates tor config file' do
+    expect(chef_run).to create_cookbook_file('/etc/tor/torrc')
+  end
+
+  it 'creates systemd service sub-directory' do
+    expect(chef_run).to create_directory('/etc/systemd/system/tor.service.d')
+  end
+
+  it 'creates tor config file for systemd' do
+    expect(chef_run).to create_cookbook_file('/etc/systemd/system/tor.service.d/start-as-root.conf')
+  end
+
+  it 'creates sysctl config' do
+    expect(chef_run).to create_cookbook_file('/etc/sysctl.d/99-sysctl.conf')
+  end
+
+  it 'notifies sysctl' do
+    resource = chef_run.cookbook_file('/etc/sysctl.d/99-sysctl.conf')
+    expect(resource).to notify('execute[sysctl reload]').to(:run).immediately
   end
 end
