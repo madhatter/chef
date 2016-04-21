c736a0f2d240197ba4214bdc16d81ea8ebfe2caf
Arvid Warnecke <madhatter@nostalgix.org>
2016-04-21 06:57:51 +0200

    [nostalgix_nginx] Fill everything from the data_bag.

--- site-cookbooks/nostalgix_nginx/recipes/default.rb
+++ site-cookbooks/nostalgix_nginx/recipes/default.rb
@@ -9,7 +9,7 @@ template '/etc/nginx/sites-available/nostalgix' do
   owner 'root'
   group 'root'
   mode '0644'
-  variables(:server_name => data_bag['server_name'])
+  variables(:data_bag => data_bag)
   action :create
 end
 
--- site-cookbooks/nostalgix_nginx/spec/default_spec.rb
+++ site-cookbooks/nostalgix_nginx/spec/default_spec.rb
@@ -2,7 +2,15 @@ require_relative '../../spec_helper.rb'
 
 describe 'nostalgix_nginx::default' do
   before do
-    stub_data_bag_item('nginx', 'nostalgix').and_return({ 'server_name' => 'gammel.org' })
+    stub_data_bag_item('nginx', 'nostalgix').and_return({
+      'server_name' => 'gammel.org',
+      'http_port' => '80',
+      'https_port' => '443',
+      'ssl_certificate' => '/some_dir/gammelcert',
+      'ssl_certificate_key' => '/some_dir/gammelkey',
+      'access_log' => '/logdir/logfile',
+      'root_dir' => '/wwwhome'
+    })
   end
 
   let(:chef_run) do
@@ -18,5 +26,11 @@ describe 'nostalgix_nginx::default' do
   it 'creates nginx config from data_bag' do
     expect(chef_run).to create_template('/etc/nginx/sites-available/nostalgix')
     expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix').with_content(/gammel\.org/)
+    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix').with_content(/80/)
+    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix').with_content(/443/)
+    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix').with_content(/\/some_dir\/gammelcert/)
+    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix').with_content(/\/some_dir\/gammelkey/)
+    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix').with_content(/\/logdir\/logfile/)
+    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix').with_content(/\/wwwhome/)
   end
 end
