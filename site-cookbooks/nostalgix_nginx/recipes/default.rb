c87cfca9a73ed82c267053ad2dbfaa4a2fc53bdf
Arvid Warnecke <madhatter@nostalgix.org>
2016-04-23 17:59:22 +0200

    [nostalgix_nginx] Make template resource more re-usable for many different sites.

--- site-cookbooks/nostalgix_nginx/recipes/default.rb
+++ site-cookbooks/nostalgix_nginx/recipes/default.rb
@@ -1,15 +1,16 @@
-data_bag = data_bag_item('nginx', 'nostalgix')
-
 package 'nginx' do
   action :install
 end
 
-template '/etc/nginx/sites-available/nostalgix' do
-  source 'site.erb'
-  owner 'root'
-  group 'root'
-  mode '0644'
-  variables(:data_bag => data_bag)
-  action :create
+node['nginx']['sites'].each do |site| 
+  data_bag = data_bag_item('nginx', site)
+  
+  template "/etc/nginx/sites-available/#{site}" do
+    source 'site.erb'
+    owner 'root'
+    group 'root'
+    mode '0644'
+    variables(:data_bag => data_bag)
+    action :create
+  end
 end
-
--- site-cookbooks/nostalgix_nginx/spec/default_spec.rb
+++ site-cookbooks/nostalgix_nginx/spec/default_spec.rb
@@ -2,7 +2,7 @@ require_relative '../../spec_helper.rb'
 
 describe 'nostalgix_nginx::default' do
   before do
-    stub_data_bag_item('nginx', 'nostalgix').and_return({
+    stub_data_bag_item('nginx', 'nostalgix.org').and_return({
       'server_name' => 'gammel.org',
       'http_port' => '80',
       'https_port' => '443',
@@ -16,6 +16,7 @@ describe 'nostalgix_nginx::default' do
   let(:chef_run) do
      ChefSpec::SoloRunner.new do |node|
        node.set['nginx']['config_dir'] = '/etc/nginx'
+       node.set['nginx']['sites'] = ['nostalgix.org']
     end.converge(described_recipe)
   end
 
@@ -24,13 +25,13 @@ describe 'nostalgix_nginx::default' do
   end
 
   it 'creates nginx config from data_bag' do
-    expect(chef_run).to create_template('/etc/nginx/sites-available/nostalgix')
-    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix').with_content(/gammel\.org/)
-    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix').with_content(/80/)
-    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix').with_content(/443/)
-    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix').with_content(/\/some_dir\/gammelcert/)
-    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix').with_content(/\/some_dir\/gammelkey/)
-    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix').with_content(/\/logdir\/logfile/)
-    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix').with_content(/\/wwwhome/)
+    expect(chef_run).to create_template('/etc/nginx/sites-available/nostalgix.org')
+    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix.org').with_content(/gammel\.org/)
+    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix.org').with_content(/80/)
+    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix.org').with_content(/443/)
+    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix.org').with_content(/\/some_dir\/gammelcert/)
+    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix.org').with_content(/\/some_dir\/gammelkey/)
+    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix.org').with_content(/\/logdir\/logfile/)
+    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix.org').with_content(/\/wwwhome/)
   end
 end
