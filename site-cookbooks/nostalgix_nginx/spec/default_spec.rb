22fb723015500915e019fab23b1102fefbbc8744
Arvid Warnecke <madhatter@nostalgix.org>
2016-04-26 20:51:24 +0200

    [nostalgix_nginx] More sites possible with different setup options.

--- site-cookbooks/nostalgix_nginx/spec/default_spec.rb
+++ site-cookbooks/nostalgix_nginx/spec/default_spec.rb
@@ -2,7 +2,6 @@ require_relative '../../spec_helper.rb'
 
 describe 'nostalgix_nginx::default' do
   before do
-    # TODO: Add more sites here
     stub_data_bag_item('nginx', 'nostalgix.org').and_return({
       'server_name' => 'gammel.org',
       'http_port' => '80',
@@ -12,12 +11,18 @@ describe 'nostalgix_nginx::default' do
       'access_log' => '/logdir/logfile',
       'root_dir' => '/wwwhome'
     })
+    stub_data_bag_item('nginx', 'share.nostalgix.org').and_return({
+      'server_name' => 'share.gammel.org',
+      'http_port' => '80',
+      'access_log' => '/logdir/share_logfile',
+      'root_dir' => '/wwwhome_share'
+    })
   end
 
   let(:chef_run) do
      ChefSpec::SoloRunner.new do |node|
        node.set['nginx']['config_dir'] = '/etc/nginx'
-       node.set['nginx']['sites'] = ['nostalgix.org']
+       node.set['nginx']['sites'] = ['nostalgix.org','share.nostalgix.org']
     end.converge(described_recipe)
   end
 
@@ -27,12 +32,37 @@ describe 'nostalgix_nginx::default' do
 
   it 'creates nginx config from data_bag' do
     expect(chef_run).to create_template('/etc/nginx/sites-available/nostalgix.org')
-    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix.org').with_content(/gammel\.org/)
-    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix.org').with_content(/80/)
-    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix.org').with_content(/443/)
-    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix.org').with_content(/\/some_dir\/gammelcert/)
-    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix.org').with_content(/\/some_dir\/gammelkey/)
-    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix.org').with_content(/\/logdir\/logfile/)
-    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix.org').with_content(/\/wwwhome/)
+    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix.org')
+      .with_content(/www\.gammel\.org/)
+    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix.org')
+      .with_content(/gammel\.org/)
+    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix.org')
+      .with_content(/80/)
+    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix.org')
+      .with_content(/443/)
+    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix.org')
+      .with_content(/\/some_dir\/gammelcert/)
+    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix.org')
+      .with_content(/\/some_dir\/gammelkey/)
+    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix.org')
+      .with_content(/\/logdir\/logfile/)
+    expect(chef_run).to render_file('/etc/nginx/sites-available/nostalgix.org')
+      .with_content(/\/wwwhome/)
+  end
+
+  it 'creates nginx config from data_bag with less options' do
+    expect(chef_run).to create_template('/etc/nginx/sites-available/share.nostalgix.org')
+    expect(chef_run).to render_file('/etc/nginx/sites-available/share.nostalgix.org')
+      .with_content(/share\.gammel\.org/)
+    expect(chef_run).not_to render_file('/etc/nginx/sites-available/share.nostalgix.org')
+      .with_content(/www\.share\.gammel\.org/)
+    expect(chef_run).not_to render_file('/etc/nginx/sites-available/share.nostalgix.org')
+      .with_content(/443/)
+    expect(chef_run).not_to render_file('/etc/nginx/sites-available/share.nostalgix.org')
+      .with_content(/ssl_certificate/)
+    expect(chef_run).to render_file('/etc/nginx/sites-available/share.nostalgix.org')
+      .with_content(/\/logdir\/share_logfile/)
+    expect(chef_run).to render_file('/etc/nginx/sites-available/share.nostalgix.org')
+      .with_content(/\/wwwhome_share/)
   end
 end
