783d100534a17362f06f41353363fffad42bf4c3
Arvid Warnecke <madhatter@nostalgix.org>
2017-02-04 18:53:29 +0100

    [spec] Updated some spec tests to remove deprecated syntax.

--- site-cookbooks/base/spec/default_spec.rb
+++ site-cookbooks/base/spec/default_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../../spec_helper.rb'
 
 describe 'base::default' do
-  let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
+  let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'arch', version: '4.5.4-1-ARCH').converge(described_recipe) }
 
   before do
     allow_any_instance_of(Chef::Recipe).to receive(:include_recipe).and_call_original
