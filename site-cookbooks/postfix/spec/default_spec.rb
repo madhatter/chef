5b3546ed7e7242a2f33ddc6f3c75d565a6804f68
Arvid Warnecke <madhatter@nostalgix.org>
2017-05-11 15:00:58 +0200

    [postfix] Specs.

--- site-cookbooks/postfix/spec/default_spec.rb
+++ site-cookbooks/postfix/spec/default_spec.rb
@@ -1,6 +1,9 @@
 require_relative '../../spec_helper.rb'
 
 describe 'postfix::default' do
+  let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'arch', version: '4.5.4-1-ARCH')
+    .converge(described_recipe) }
+
   it 'installs postfix' do
     expect(chef_run).to install_package('postfix')
   end
