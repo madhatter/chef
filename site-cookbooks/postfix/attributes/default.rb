808fc7926a99ecc79e32df957266dbc024277127
Arvid Warnecke <madhatter@nostalgix.org>
2017-09-14 15:19:04 +0200

    [postfix] specs added.

--- site-cookbooks/postfix/attributes/default.rb
+++ site-cookbooks/postfix/attributes/default.rb
@@ -1,2 +1,2 @@
 node.default['postfix']['server_name'] = 'mail.nostalgix.org'
-node.default['postfix']['user'] = 'madhatterle'
+node.default['postfix']['user'] = 'madhatter'
--- site-cookbooks/postfix/spec/default_spec.rb
+++ site-cookbooks/postfix/spec/default_spec.rb
@@ -4,6 +4,7 @@ describe 'postfix::default' do
   let(:chef_run) do
     ChefSpec::SoloRunner.new(platform: 'arch', version: '4.5.4-1-ARCH') do |node|
       node.normal['postfix']['server_name'] = 'mail.horst.org'
+      node.normal['postfix']['user'] = 'herbert'
     end.converge(described_recipe)
   end
 
@@ -19,5 +20,11 @@ describe 'postfix::default' do
     expect(chef_run).to create_template('/etc/postfix/main.cf')
     expect(chef_run).to render_file('/etc/postfix/main.cf')
       .with_content(/mail\.horst\.org/)
+  end
+
+  it 'creates updated alias file' do
+    expect(chef_run).to create_template('/etc/postfix/aliases')
+    expect(chef_run).to render_file('/etc/postfix/aliases')
+      .with_content(/herbert/)
   end
 end
