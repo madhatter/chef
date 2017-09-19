4ed4fa75277820de2d5c3637463cc10cc16dc5e1
Arvid Warnecke <madhatter@nostalgix.org>
2017-09-19 07:16:17 +0200

    [postfix] Trying to copy example configuration for dovecot.

--- site-cookbooks/postfix/recipes/dovecot.rb
+++ site-cookbooks/postfix/recipes/dovecot.rb
@@ -2,6 +2,15 @@ package 'dovecot' do
   action :install
 end
 
+if ::Dir['/etc/dovecot/conf.d/*'].empty?
+  ruby_block "Copy example config files if there are no configuration files yet" do
+    block do
+      ::FileUtils.cp '/usr/share/doc/dovecot/example-config/dovecot.conf', '/etc/dovecot'
+      ::FileUtils.cp '/usr/share/doc/dovecot/example-config/conf.d/*', '/etc/dovecot/conf.d'
+    end
+  end
+end
+
 #service 'dovecot' do
 #  action [:enable, :start]
 #end
