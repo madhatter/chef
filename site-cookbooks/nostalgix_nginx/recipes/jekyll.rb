ff3d296d488e555cf258b385595f31292e4904a1
Arvid Warnecke <madhatter@nostalgix.org>
2017-11-07 13:15:24 +0100

    [node] Add shared subdomain to brutal

---
+++ site-cookbooks/nostalgix_nginx/recipes/jekyll.rb
@@ -0,0 +1,15 @@
+user 'deployer' do
+  gid 'wheel'
+  home "/home/#{name}"
+  shell '/bin/zsh'
+  manage_home true
+end
+
+directory '/home/deployer' do
+  owner 'deployer'
+  group 'users'
+  mode '0700'
+  action :create
+end
+
+
